da1a787084755e3759e1d6d39052f14cfd738018
Prafull <prafullpandey614@gmail.com>
2022-09-25 01:56:37 +0530

    Single View Function Added

--- review/urls.py
+++ review/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path("",views.ReviewView.as_view()),
     path("thank-you",views.ThankYouView.as_view()),
     path("all-data",views.AllReviewsView.as_view(), name="all-data"),
+    path("review/<int:id>",views.SingleReviewView.as_view()),
 ]
--- review/views.py
+++ review/views.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
+# from msilib.schema import ListView
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from matplotlib.style import context
 from .forms import ReviewForm
 from .models import Review
 from django.views import View 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView , ListView
 # # Create your views here.
 # def review(models.Model):
 #     pass
@@ -55,14 +57,22 @@ class ThankYouView(TemplateView):
         context =  super().get_context_data(**kwargs) #super method keeps all the previous data saved from parent class TemplateView
         context["message"] = "This Context in templateView is Working Properly"
         return context
-class AllReviewsView(TemplateView):
+class AllReviewsView(ListView):
     template_name = "reviews/review_list.html"
-   
+    model = Review
+    context_object_name = "data"
+    # def get_context_data(self, **kwargs) :
+    #     context =  super().get_context_data(**kwargs)
+    #     data = Review.objects.all()
+    #     context["data"] = data
+    #     return context
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_data.html"
     def get_context_data(self, **kwargs) :
         context =  super().get_context_data(**kwargs)
-        data = Review.objects.all()
-        context["data"] = data
+        id = kwargs["id"]
+        selected_person = Review.objects.get(pk=id)
+        context["data"] = selected_person
         return context
-    
 # def thank_you(request):
 #     return render(request,'reviews/formd.html')
